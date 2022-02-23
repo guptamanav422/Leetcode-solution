7f9d80caeca526cb2b4725d5638792e691cda3dd
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-02-23 13:27:05 +0530

    Added solution - LeetHub

---
+++ Binary Tree to BST - GFG/binary-tree-to-bst.java
@@ -0,0 +1,144 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+import java.util.LinkedList; 
+import java.util.Queue; 
+import java.io.*;
+import java.util.*;
+
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+
+class GFG {
+    
+    static Node buildTree(String str){
+        
+        if(str.length()==0 || str.charAt(0)=='N'){
+            return null;
+        }
+        
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+        
+        Queue<Node> queue = new LinkedList<>(); 
+        
+        queue.add(root);
+        // Starting from the second element
+        
+        int i = 1;
+        while(queue.size()>0 && i < ip.length) {
+            
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+                
+            // Get the current node's value from the string
+            String currVal = ip[i];
+                
+            // If the left child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+                
+            // For the right child
+            i++;
+            if(i >= ip.length)
+                break;
+                
+            currVal = ip[i];
+                
+            // If the right child is not null
+            if(!currVal.equals("N")) {
+                    
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+                    
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+        
+        return root;
+    }
+    static void printInorder(Node root){
+        if(root == null)
+            return;
+        printInorder(root.left);
+        System.out.print(root.data+" ");
+        printInorder(root.right);
+    }
+    public static void main (String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int t=Integer.parseInt(br.readLine());
+        while(t > 0){
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Solution g = new Solution();
+            Node rootA = g.binaryTreeToBST(root);
+            printInorder(rootA);
+            System.out.println();
+            t--;
+        }
+    }
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+/*Structure of the node class is
+class Node
+{
+    int data;
+    Node left, right;
+    Node(int item){
+        data = item;
+        left = right = null;
+    }
+} */
+
+
+class Solution
+{
+    // The given root is the root of the Binary Tree
+    // Return the root of the generated BST
+    Node binaryTreeToBST(Node root)
+    {
+       List<Integer> list=new LinkedList<>();
+       store(root,list);
+       Collections.sort(list);
+       convertToBst(root,list);
+       return root;
+    }
+    void store(Node root,List<Integer> list)
+    {
+        if(root==null) return;
+        list.add(root.data);
+        store(root.left,list);
+        store(root.right,list);
+    }
+    void convertToBst(Node root,List<Integer> list)
+    {
+        if(root==null) return;
+        convertToBst(root.left,list);
+        root.data=list.remove(0);
+        convertToBst(root.right,list);
+    }
+}
+ 
