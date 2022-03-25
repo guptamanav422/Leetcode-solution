ab376f6536aa12d0c6c5d06b24afcb529af7c4af
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-03-25 10:12:08 +0530

    Time: 231 ms (24.15%), Space: 68.5 MB (50.87%) - LeetHub

--- 204-count-primes/204-count-primes.java
+++ 204-count-primes/204-count-primes.java
@@ -2,16 +2,13 @@ class Solution {
     public int countPrimes(int n) {
         boolean dp[]=new boolean[n];
         Arrays.fill(dp,true);
+        int ans=0;
         for(int i=2;i<n;i++){
             if(!dp[i]) continue;
-            
+            ans++;
             for(int j=i*2;j<n;j+=i){
                 dp[j]=false;
             }
-        }
-        int ans=0;
-        for(int i=2;i<n;i++){
-            if(dp[i]) ans++;
         }
         return ans;
     }
