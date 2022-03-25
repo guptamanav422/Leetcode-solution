2d4c5b0a61e2c5d0add1f37f8b3289c7e96b2ff0
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-03-25 09:39:14 +0530

    Time: 2 ms (78.58%), Space: 42.3 MB (40.68%) - LeetHub

--- 1029-two-city-scheduling/1029-two-city-scheduling.java
+++ 1029-two-city-scheduling/1029-two-city-scheduling.java
@@ -4,21 +4,18 @@ class Solution {
     public int twoCitySchedCost(int[][] costs) {
         
         int n=costs.length;
-        dp=new int[n/2+1][n/2+1];
-        ans=Integer.MAX_VALUE;
-        find(costs,0,0,n/2,0);
-        return ans;
-    }
-    void find(int arr[][],int c1,int c2,int n,int sum){
-        
-        if(sum>ans) return;
-        if(c1+c2==(2*n)){
-            ans=Math.min(ans,sum);
-            return;
+        int ref[]=new int[n];
+        int ans=0;
+        for(int i=0;i<n;i++){
+            ref[i]=costs[i][1]-costs[i][0];
+            ans+=costs[i][0];
         }
-        if(dp[c1][c2]!=0 && dp[c1][c2]<=sum) return;
-        dp[c1][c2]=sum;
-        if(c1<n) find(arr,c1+1,c2,n,sum+arr[c1+c2][0]);
-        if(c2<n) find(arr,c1,c2+1,n,sum+arr[c1+c2][1]);
+        System.out.println(ans);
+        Arrays.sort(ref);
+        for(int i=0;i<n/2;i++){
+            ans+=ref[i];
+            // System.out.println(ref[i])
+        }
+        return ans;
     }
 }
