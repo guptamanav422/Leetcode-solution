5c17bdbd71072d544798f81a2a56e0a6e3dc03d1
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-03-30 17:44:09 +0530

    Time: 87 ms (74.70%), Space: 42.9 MB (89.67%) - LeetHub

---
+++ 30-substring-with-concatenation-of-all-words/30-substring-with-concatenation-of-all-words.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<Integer> findSubstring(String s, String[] words) {
+        
+        int len=s.length(),n=words.length;
+        List<Integer> list=new ArrayList<>();
+        Map<String,Integer> map=new HashMap<>();
+        for(String str:words){
+            map.put(str,map.getOrDefault(str,0)+1);
+        }
+        int one=words[0].length();
+        for(int i=0;i<=(len-one*n);i++){
+            Map<String,Integer> temp=new HashMap<>();
+            int c=0;
+            int j=i;
+            while(c<n){
+                String str=s.substring(i,i+one);
+                temp.put(str,temp.getOrDefault(str,0)+1);
+                
+                if(temp.get(str)>map.getOrDefault(str,0)) break;
+                c++;
+                i=i+one;
+            }
+            i=j;
+            if(c==n) list.add(i);
+        }
+        return list;
+    }
+}
