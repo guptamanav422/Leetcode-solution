604d6c57690f01947a80997e498ae7d4739d92b2
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-05-20 21:45:43 +0530

    Time: 1 ms (52.25%), Space: 42.4 MB (41.74%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] arr) {
+        int n=arr.length;
+        int m=arr[0].length;
+        
+        int dp[][]=new int[n][m];
+        for(int i=n-1;i>=0;i--){
+            for(int j=m-1;j>=0;j--){
+                if(arr[i][j]==1) continue;
+                
+                if(i==n-1 && j==m-1) dp[i][j]=1;
+                else if(i==n-1){
+                    dp[i][j]=dp[i][j+1];
+                }
+                else if(j==m-1){
+                    dp[i][j]=dp[i+1][j];
+                }
+                else{
+                    dp[i][j]=dp[i+1][j] + dp[i][j+1];
+                }
+            }
+        }
+        return dp[0][0];
+    }
+}
