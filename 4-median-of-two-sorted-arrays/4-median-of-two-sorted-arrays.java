d1de97661d5423ed6b7b0182cbf33be8fe956120
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-07-06 18:07:10 +0530

    Time: 2 ms (100.00%), Space: 43.4 MB (87.15%) - LeetHub

--- 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/4-median-of-two-sorted-arrays.java
@@ -2,31 +2,34 @@ class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
         
         int n=nums1.length,m=nums2.length;
-        
         if(n>m) return findMedianSortedArrays(nums2,nums1);
+        
+        
         int s=0,e=n;
         while(s<=e){
             
-            int mid1=(s+e)/2;
+            int m1=(s+e)/2;
             
-            int l1=(mid1==0?Integer.MIN_VALUE:nums1[mid1-1]);
-            int r1=(mid1==n?Integer.MAX_VALUE:nums1[mid1]);
+            int l1=(m1==0)?(Integer.MIN_VALUE):(nums1[m1-1]);
+            int r1=(m1==n)?(Integer.MAX_VALUE):(nums1[m1]);
             
-            int mid2=(n+m)/2-(mid1);
+            int m2=(n+m)/2-m1;
             
-            int l2=(mid2==0?Integer.MIN_VALUE:nums2[mid2-1]);
-            int r2=(mid2==m?Integer.MAX_VALUE:nums2[mid2]);
-            // System.out.println(l1+" "+r1);
-            // System.out.println(l2+" "+r2);
+            int l2=(m2==0)?(Integer.MIN_VALUE):(nums2[m2-1]);
+            int r2=(m2==m)?(Integer.MAX_VALUE):(nums2[m2]);
+            
             if(l1<=r2 && l2<=r1){
                 
                 if((n+m)%2==0){
-                    return ((double)(Math.max(l1,l2)+Math.min(r1,r2)))/2.0;
+                    
+                    return (double)((double)(Math.max(l1,l2)) +(double)(Math.min(r1,r2)))/2.0;
                 }
-                else return Math.min(r1,r2);
+                else return (double)(Math.min(r1,r2));
             }
-            else if(l1>r2) e=mid1;
-            else s=mid1+1;
+            else if(l1>r2){
+                e=m1-1;
+            }
+            else s=m1+1;
         }
         return 0.0;
     }
