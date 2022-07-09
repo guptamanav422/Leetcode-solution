a33f18ae760380916037993c40a9dca874425190
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-07-09 11:45:54 +0530

    Time: 10 ms (90.27%), Space: 41.5 MB (95.78%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.java
+++ 9-palindrome-number/9-palindrome-number.java
@@ -1,9 +1,19 @@
 class Solution {
+    int y;
     public boolean isPalindrome(int x) {
-        
+       y=x;
         if(x<0) return false;
+       return check(x);
+    }
+    boolean check(int x){
         
-        String a=new StringBuilder(x+"").reverse().toString();
-        return a.equals(x+"");
+        if(x==0) return true;
+        
+        boolean ans=check(x/10);
+        
+        boolean ans1=x%10==y%10;
+        y/=10;
+        return ans & ans1;
+        
     }
 }
