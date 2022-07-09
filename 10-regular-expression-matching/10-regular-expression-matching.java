c478ab828691abdf80d67ea5b5eb435da333296d
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-07-09 13:34:04 +0530

    Time: 4 ms (70.63%), Space: 42.7 MB (59.48%) - LeetHub

--- 10-regular-expression-matching/10-regular-expression-matching.java
+++ 10-regular-expression-matching/10-regular-expression-matching.java
@@ -6,19 +6,25 @@ class Solution {
         boolean dp[][]=new boolean[n+1][m+1];
         for(int i=0;i<=n;i++){
             for(int j=0;j<=m;j++){
+                
                 if(i==0 && j==0) dp[i][j]=true;
                 else if(i==0){
-                    if(p.charAt(j-1)=='*' ) dp[i][j]=dp[i][j-2];
+                    dp[i][j]=p.charAt(j-1)=='*' && dp[i][j-2];
                 }
-                else if(i!=0 && j!=0){
-                    if(p.charAt(j-1)==s.charAt(i-1) || p.charAt(j-1)=='.') dp[i][j]=dp[i-1][j-1];
-                    else if(p.charAt(j-1)=='*'){
-                        if(p.charAt(j-2)=='.' || p.charAt(j-2)==s.charAt(i-1)) dp[i][j]=dp[i-1][j];
-                        dp[i][j]=dp[i][j] || dp[i][j-1] || dp[i][j-2];
-                    }
+                else if(j!=0){
+                    if(s.charAt(i-1) == p.charAt(j-1) || p.charAt(j-1)=='.') dp[i][j]=dp[i-1][j-1];
+                    else if(p.charAt(j-1)=='*') dp[i][j]=dp[i][j-1] || dp[i][j-2] || ((p.charAt(j-2)==s.charAt(i-1) || p.charAt(j-2)=='.') && dp[i-1][j]);
                 }
+               
             }
         }
+//         for(int i=0;i<=n;i++){
+//             for(int j=0;j<=m;j++){
+                
+//                System.out.print(dp[i][j]+" ");
+//             }
+//             System.out.println();
+//         }
         return dp[n][m];
     }
 }
