cdc1b005f472b4dd65831b5ca7c1c56f9fe03a41
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-07-10 09:52:26 +0530

    Time: 73 ms (100.00%), Space: 18.9 MB (100.00%) - LeetHub

---
+++ 2337-move-pieces-to-obtain-a-string/2337-move-pieces-to-obtain-a-string.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool canChange(string st, string tar) {
+        int n=tar.length(),m=st.length();
+        if(n!=m) return false;
+    
+        int a=0,b=0,i=0,j=0;
+        while(i<n && j<n){
+            
+            while(i<n && tar[i]=='_') i++;
+            while(j<n && st[j]=='_') j++;
+            
+            if(i==n || j==n){
+                return i==n && j==n;
+            }
+            
+            if(tar[i]!=st[j]) return false;
+            
+            if(tar[i]=='L'){
+                if(j<i) return false;
+            }
+            else{
+                if(i<j) return false;
+            }
+            
+            i++;
+            j++;
+        }
+        return true;
+    }
+};
