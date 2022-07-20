c4180637e67271e683a3a0918b9aaf22dbf9ab02
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-07-20 14:01:36 +0530

    Time: 327 ms (5.08%), Space: 96 MB (37.29%) - LeetHub

---
+++ 2069-walking-robot-simulation-ii/2069-walking-robot-simulation-ii.java
@@ -0,0 +1,70 @@
+class Robot {
+    
+    int arr[][];
+    int x=0,y=0,h=0,w=0;
+    String dir="East";
+    public Robot(int width, int height) {
+        arr=new int[height][width];
+        h=height;
+        w=width;
+        // System.out.println(width+" "+height);
+    }
+    
+    public void step(int num) {
+        
+        long temp=(h-1)*2 +(w-1)*2;
+        num=(int)((long)num%(temp));
+        if (num == 0) num = w * 2 + h * 2 - 4;
+        
+        if(num==0){
+            dir="South";
+        }
+        while(num-->0){
+            if(dir.equals("East")){
+                if(y==w-1){
+                    x++;
+                    dir="North";
+                }
+                else y++;
+            }
+            else if(dir.equals("North")){
+                if(x==h-1){
+                    y--;
+                    dir="West";
+                }
+                else x++;
+            }
+            else if(dir.equals("West")){
+                if(y==0){
+                    x--;
+                    dir="South";
+                }
+                else y--;
+            }
+            else if(dir.equals("South")){
+                if(x==0){
+                    y++;
+                    dir="East";
+                }
+                else x--;
+            }
+        }
+        
+    }
+    
+    public int[] getPos() {
+        return new int[]{y,x};
+    }
+    
+    public String getDir() {
+        return dir;
+    }
+}
+
+/**
+ * Your Robot object will be instantiated and called as such:
+ * Robot obj = new Robot(width, height);
+ * obj.step(num);
+ * int[] param_2 = obj.getPos();
+ * String param_3 = obj.getDir();
+ */
