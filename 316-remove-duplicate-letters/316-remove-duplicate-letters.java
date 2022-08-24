0e4f990c59ec4a07e4ad646315436ba47769bcb9
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-08-24 13:38:27 +0530

    Time: 5 ms (63.51%), Space: 43.1 MB (44.77%) - LeetHub

--- 316-remove-duplicate-letters/316-remove-duplicate-letters.java
+++ 316-remove-duplicate-letters/316-remove-duplicate-letters.java
@@ -1,29 +1,30 @@
-class Solution
-{
-    public String removeDuplicateLetters(String s)
-    {
-        int arr[]=new int[26];
-        for(char ch:s.toCharArray())
-        {
-            arr[ch-'a']++; 
+class Solution {
+    public String removeDuplicateLetters(String s) {
+        
+        boolean vis[]=new boolean[26];
+        int fre[]=new int[26];
+        for(char ch:s.toCharArray()){
+            fre[ch-'a']++;
         }
         Stack<Character> st=new Stack<>();
-        boolean visited[]=new boolean[26];
-        for(char ch:s.toCharArray())
-        {
-            int index=ch-'a';
-            arr[index]--;
-            if(visited[index]) continue;
-            while(!st.isEmpty() && st.peek()>ch && arr[st.peek()-'a']!=0)
-            {
-                visited[st.pop()-'a']=false;
+        for(char ch:s.toCharArray()){
+            
+            while(!st.isEmpty() && !vis[ch-'a'] && st.peek()>=ch && fre[st.peek()-'a']>0){
+                vis[st.pop()-'a']=false;
             }
-            visited[index]=true;
-            st.push(ch);
+            // System.out.print(st.isEmpty()?-1:((char)st.peek()));
+            // System.out.print(" ");
+            fre[ch-'a']--;
+            if(!vis[ch-'a']){          
+                vis[ch-'a']=true;
+                st.push(ch);   
+            }
+            // System.out.println((Character)st.peek());
         }
         StringBuilder sb=new StringBuilder();
-        while(!st.isEmpty())
+        while(!st.isEmpty()){
             sb.append(st.pop());
+        }
         return sb.reverse().toString();
     }
 }
