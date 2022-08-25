fc3d23467ad77d8746e4074b12984c1ce9438d57
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-08-25 22:43:29 +0530

    Time: 5 ms (74.47%), Space: 46.6 MB (53.26%) - LeetHub

---
+++ 383-ransom-note/383-ransom-note.java
@@ -0,0 +1,15 @@
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int arr[]=new int[26];
+        for(char ch: magazine.toCharArray()){
+            arr[ch-'a']++;
+        }
+        
+        for(char ch:ransomNote.toCharArray()){
+            if(--arr[ch-'a']<0) return false;
+        }
+        
+        return true;
+    }
+}
