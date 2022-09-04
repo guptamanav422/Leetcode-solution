ae35e83116ff95579b727f10b5e1077b90b57eff
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-04 16:22:33 +0530

    Time: 0 ms (100.00%), Space: 41 MB (90.57%) - LeetHub

--- 48-rotate-image/48-rotate-image.java
+++ 48-rotate-image/48-rotate-image.java
@@ -1,28 +1,26 @@
-class Solution
-{
-    public void rotate(int[][] arr)
-    {
-        int n=arr.length;
-        for(int i=0;i<n;i++)
-        {
-            for(int j=i+1;j<n;j++)
-            {
-                 int temp=arr[i][j];
-                 arr[i][j]=arr[j][i];
-                 arr[j][i]=temp;
+class Solution {
+    public void rotate(int[][] matrix) {
+        
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
             }
         }
-        int i=0,j=n-1;
-        while(i<j)
-        {
-            for(int k=0;k<n;k++)
-            {
-                int temp=arr[k][i];
-                arr[k][i]=arr[k][j];
-                arr[k][j]=temp;
+        
+        for(int i=0;i<n;i++){
+            
+            int s=0,e=n-1;
+            while(s<e){
+                int temp=matrix[i][s];
+                matrix[i][s]=matrix[i][e];
+                matrix[i][e]=temp;
+                s++;
+                e--;
             }
-            i++;
-            j--;
         }
     }
 }
