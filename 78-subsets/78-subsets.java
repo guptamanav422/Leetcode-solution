ac65a412274e5d359b5a34278baa8728f5d03259
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-06 17:26:40 +0530

    Time: 3 ms (9.54%), Space: 43.5 MB (34.46%) - LeetHub

--- 78-subsets/78-subsets.java
+++ 78-subsets/78-subsets.java
@@ -7,11 +7,15 @@ class Solution {
 }
 
 private void backtrack(List<List<Integer>> list , List<Integer> tempList, int [] nums, int start){
-    list.add(new ArrayList<>(tempList));
-    for(int i = start; i < nums.length; i++){
-        tempList.add(nums[i]);
-        backtrack(list, tempList, nums, i + 1);
-        tempList.remove(tempList.size() - 1);
+    
+    if(start==nums.length){
+        list.add(new ArrayList<>(tempList));
+        return;
     }
+    
+    backtrack(list, tempList, nums, start + 1);
+    tempList.add(nums[start]);
+    backtrack(list, tempList, nums, start + 1);
+    tempList.remove(tempList.size() - 1);
 }
 }
