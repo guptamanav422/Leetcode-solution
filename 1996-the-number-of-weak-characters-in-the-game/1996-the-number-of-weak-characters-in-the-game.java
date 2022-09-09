54741c8398b13b7a25d93b81de7f12f127ef0fb8
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-09 11:13:51 +0530

    Time: 168 ms (52.09%), Space: 87.5 MB (97.19%) - LeetHub

--- 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.java
@@ -1,24 +1,19 @@
 class Solution {
     public int numberOfWeakCharacters(int[][] arr) {
         
-        Arrays.sort(arr,(int a[],int b[])->(a[0]==b[0])?(a[1]-b[1]):(a[0]-b[0]));
+        Arrays.sort(arr,(int a[],int b[])->(a[0]==b[0])?(b[1]-a[1]):(a[0]-b[0]));
         
         int n=arr.length,ans=0;
-        PriorityQueue<int []> pq=new PriorityQueue<>((int a[],int b[])->(a[1]==b[1])?(a[0]-b[0]):(a[1]-b[1]));
-        PriorityQueue<int []> temp=new PriorityQueue<>((int a[],int b[])->(a[1]==b[1])?(a[0]-b[0]):(a[1]-b[1]));
+        PriorityQueue<int []> pq=new PriorityQueue<>((int a[],int b[])->(a[1]-b[1]));
+        // PriorityQueue<int []> temp=new PriorityQueue<>((int a[],int b[])->(a[1]==b[1])?(a[0]-b[0]):(a[1]-b[1]));
         pq.add(arr[0]);
         for(int i=1;i<n;i++){
-            
-            if(temp.size()!=0 && temp.peek()[0]!=arr[i][0]){
-                pq.addAll(temp);
-                temp=new PriorityQueue<>((int a[],int b[])->(a[1]==b[1])?(a[0]-b[0]):(a[1]-b[1]));
-            }
             while(!pq.isEmpty() && arr[i][1]>pq.peek()[1]){
                 ans++;
                 pq.remove();
             }
             
-             temp.add(arr[i]);
+            pq.add(arr[i]);
         }
         return ans;
     }
