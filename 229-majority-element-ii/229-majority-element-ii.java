43612925d86cb1c1a2500a592a92d09715cf09c3
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-19 11:36:45 +0530

    Time: 4 ms (67.11%), Space: 49.9 MB (45.43%) - LeetHub

--- 229-majority-element-ii/229-majority-element-ii.java
+++ 229-majority-element-ii/229-majority-element-ii.java
@@ -1,46 +1,34 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
         
-        int n=nums.length;
-        int num1=Integer.MIN_VALUE,num2=Integer.MIN_VALUE;
-        int count1=0,count2=0;
         
-        for(int a:nums){
-            if(num1==a){
-                num1=a;
-                count1++;
+        int a=Integer.MAX_VALUE,b=Integer.MAX_VALUE,m1=0,m2=0,n=nums.length;
+        
+        for(int num:nums){
+            if(num==a) m1++;
+            else if(num==b) m2++;
+            else if(a==Integer.MAX_VALUE || m1==0){
+                a=num;
+                m1=1;
             }
-            else if(num2==a){
-                num2=a;
-                count2++;
-            }
-            else if(count1==0){
-                count1=1;
-                num1=a;
-            }
-            else if(count2==0){
-                count2=1;
-                num2=a;
+            else if(b==Integer.MAX_VALUE || m2==0){
+                b=num;
+                m2=1;
             }
             else{
-                count1--;
-                count2--;
+                m1--;
+                m2--;
             }
         }
-        // System.out.println(num1+" "+num2);
         
-        count1=0;
-        count2=0;
-        
-        for(int a:nums){
-            if(a==num1) count1++;
-            if(a==num2) count2++;
+        m1=m2=0;
+        for(int num:nums){
+            if(num==a) m1++;
+            else if(num==b) m2++;
         }
-        
-        List<Integer> list=new ArrayList<>();
-        if(count1>(n/3)) list.add(num1);
-        if(count2>(n/3)) list.add(num2);
-        
-        return list;
+        List<Integer> ans=new ArrayList<>();
+        if(m1>n/3) ans.add(a);
+        if(m2>n/3) ans.add(b);
+        return ans;
     }
 }
