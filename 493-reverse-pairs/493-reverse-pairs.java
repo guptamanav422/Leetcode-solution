f381cdca3b56c69ebdc23783d9e1fed857bb19d8
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-19 14:21:59 +0530

    Time: 101 ms (48.70%), Space: 74.1 MB (54.73%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.java
+++ 493-reverse-pairs/493-reverse-pairs.java
@@ -19,36 +19,18 @@ class Solution {
     }
     void merge(int arr[],int s,int m,int e){
         
-        int n1=m-s+1,n2=e-m;
-        long a1[]=new long[n1],a2[]=new long[n2];
         
-        for(int i=s;i<=e;i++){
-            if(i<=m) a1[i-s]=arr[i];
-            else a2[i-m-1]=arr[i];
-        }
-        for(int j=0;j<n2;j++){
-            int idx=Arrays.binarySearch(a1,2*a2[j]);
-            if(idx<0) idx=-(idx+1);
-            else while(idx<a1.length && a1[idx]==2*a2[j]) idx++;
-            ans+=n1-idx;
-        }
-        int i=0,j=0,k=s;
-        while(i<n1 && j<n2){
+        int i=s,j=m+1;
+        while(i<=m && j<=e){
             
-            if(a1[i]<a2[j]){
-                arr[k++]=(int)a1[i++];
+            boolean ok=((long)arr[i]>(long)((long)arr[j]*2));
+            
+            if(ok){
+                ans+=m-i+1;
+                j++;
             }
-            else{arr[k++]=(int)a2[j++];
-            }
+            else i++;
         }
-        
-        while(i<n1){
-            arr[k++]=(int)a1[i++];
-        }
-        
-        while(j<n2){
-            arr[k++]=(int)a2[j++];
-        }
-        // System.out.println(s+" "+m+" "+e+" "+ans);
+        Arrays.sort(arr,s,e+1);
     }
 }
