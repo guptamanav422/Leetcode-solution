f88569d43cdd6d5066c28b747a8e99fcb449c2d5
guptamanav422 <86852187+guptamanav422@users.noreply.github.com>
2022-09-20 11:02:07 +0530

    Time: 439 ms (5.04%), Space: 78.7 MB (33.75%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,16 +1,30 @@
 class Solution {
+    int dp[][];
+    int ans=0;
     public int findLength(int[] nums1, int[] nums2) {
         int n=nums1.length,m=nums2.length;
-        int dp[][]=new int[n+1][m+1];
-        int ans=0;
-        for(int i=1;i<=n;i++)
-        {
-            for(int j=1;j<=m;j++)
-            {
-                dp[i][j]=nums1[i-1]==nums2[j-1]?dp[i-1][j-1]+1:0;
-                if(dp[i][j]>ans) ans=dp[i][j];
-            }
+        dp=new int[n][m];
+        for(int i=0;i<n;i++){
+            Arrays.fill(dp[i],-1);
         }
+        find(nums1,nums2,0,0);
         return ans;
+    }
+    int find(int a1[],int a2[],int i,int j){
+        
+        if(i==a1.length || j==a2.length) return 0;
+        
+        if(dp[i][j]!=-1) return dp[i][j];
+        
+        // dp[i][j]=Math.max();
+        
+        int temp=0;
+        if(a1[i]==a2[j]){
+            temp=find(a1,a2,i+1,j+1)+1;
+            ans=Math.max(ans,temp);
+        }
+        find(a1,a2,i+1,j);
+        find(a1,a2,i,j+1);
+        return dp[i][j]=temp;
     }
 }
